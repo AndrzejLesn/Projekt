5c43b4d5adcebdbf38bd2a1dded500ef5b59be02
AndrzejLesn <andrzej_lesniak@wp.tv>
2016-04-18 10:05:33 +0200

    wiecej jebanych git ignore

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'rYeSgr8D-xVX4Ynnn1Q0608RbJ5YbgRz',
+        ],
+    ],
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'yqgPkPpGM-3nkVQkTq1jIVHcNspli6zt',
+        ],
+    ],
+];
